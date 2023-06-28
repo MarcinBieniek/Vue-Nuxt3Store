f626428291f94e309b6d9b4e370558b0eb52b308
Marcin Bieniek <marcingim@gmail.com>
2023-06-28 11:14:39 +0200

    1-4 Add dynamic routes structure

---
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>Lesson</h1>
+    <p>This is a lesson</p>
+    <p>This is a chapter name: {{ $route.params.chapterSlug }}</p>
+    <p>This is a lesson name: {{ $route.params.lessonSlug }}</p>
+  </div>
+</template>
--- pages/course/lesson.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h1>Lesson</h1>
-    <p>This is a lesson</p>
-  </div>
-</template>
