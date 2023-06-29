80dc3d8230210f1cf2f87f80617c72b5a7488e4c
Marcin Bieniek <marcingim@gmail.com>
2023-06-29 11:37:19 +0200

    1-11 Add useState to store lessons status

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -25,12 +25,15 @@
       :videoId="lesson.videoId"
     />
     <p>{{ lesson.text }}</p>
+    <LessonCompleteButton
+      :model-value="isLessonComplete"
+      @update:model-value="toggleComplete"
+    />
   </div>
 </template>
 
 <script setup>
   const course = useCourse()
-
   const route = useRoute()
 
   const chapter = computed(() => {
@@ -50,6 +53,36 @@
   useHead({
     title
   })
+
+  const progress = useState('progress', () => {
+    return [];
+  });
+
+  const isLessonComplete = computed(() => {
+    if (!progress.value[chapter.value.number - 1]) {
+      return false;
+    }
+    if (
+      !progress.value[chapter.value.number - 1][
+        lesson.value.number - 1
+      ]
+    ) {
+      return false;
+    }
+    return progress.value[chapter.value.number - 1][
+      lesson.value.number - 1
+    ];
+  });
+
+  const toggleComplete = () => {
+    if (!progress.value[chapter.value.number - 1]) {
+      progress.value[chapter.value.number - 1] = [];
+    }
+    progress.value[chapter.value.number - 1][
+      lesson.value.number - 1
+    ] = !isLessonComplete.value;
+  };
+
 
 </script>
 
